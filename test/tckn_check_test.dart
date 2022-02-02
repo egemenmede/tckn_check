1f0537ecc0222057a1905eef8c665435ed34391a
Egemen MEDE <egemenmede@gmail.com>
2022-02-02 16:38:28 +0300

    Update tckn_check_test.dart

--- test/tckn_check_test.dart
+++ test/tckn_check_test.dart
@@ -5,27 +5,27 @@ import 'package:tckn_check/tckn_check.dart';
 void main() {
   test('geçerli tckn bilgisinin girilmesi', () {
     final tckn = Tckn();
-    expect(tckn.check("29482514386"), true);
+    expect(tckn.check("62468678658"), true);
   });
 
   test('geçersiz tckn: kural-1 ihlali', () {
     final tckn = Tckn();
-    expect(tckn.check("2A482514386"), false);
+    expect(tckn.check("6A468678658"), false);
   });
 
   test('geçersiz tckn: kural-2 ihlali', () {
     final tckn = Tckn();
-    expect(tckn.check("9482514386"), false);
+    expect(tckn.check("2468678658"), false);
   });
 
   test('geçersiz tckn: kural-4 ihlali', () {
     final tckn = Tckn();
-    expect(tckn.check("28482514386"), false);
+    expect(tckn.check("61468678658"), false);
   });
 
   test('geçersiz tckn: kural-5 ihlali', () {
     final tckn = Tckn();
-    expect(tckn.check("28482514386"), false);
+    expect(tckn.check("61468678658"), false);
   });
 
 }
