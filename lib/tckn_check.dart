df99b3df4ac648cd99dd19c964d7fc894dcfa0ea
Egemen MEDE <egemenmede@gmail.com>
2022-02-02 16:03:39 +0300

    Rule 3,4,5 Added

--- lib/tckn_check.dart
+++ lib/tckn_check.dart
@@ -16,23 +16,95 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 
-  bool checkTCKN(int value) {
+  final int chkCount = 10;
+  final int chrCount = 11;
+
+  bool checkTCKN(String value) {
+    bool returnStatus = true;
+
     // Kural-1: Tüm karakterleri rakam olmalıdır.
-    if (value.isNaN) return false;
+    if (value.contains(RegExp(r'[A-Z,a-z,ü,Ü,ö,Ö,ğ,Ğ,i,İ,ç,Ç,ş,Ş]'))) {
+      returnStatus = false;
+      print("Kural-1: "+ returnStatus.toString());
+    }
 
     //Kural-2: TC Kimlik numarası 11 basamaktan oluşmalıdır.
-    if (value.toString().length >= 11) return false;
+    if (value.toString().length != 11) {
+      returnStatus = false;
+      print("Kural-2: "+ returnStatus.toString());
+    }
 
-    return true;
+    // Kural-3: İlk hanesi 0 olamaz.
+    if (value.toString().substring(0,1) == "0"){
+      returnStatus = false;
+      print("Kural-3: "+ returnStatus.toString());
+    }
+
+    // Kural-4: İlk 9 basamak arasındaki algoritma, 10. basamağı vermelidir.
+    if(getRuleFourStatus(value) == false){
+      returnStatus = false;
+      print("Kural-4: "+ returnStatus.toString());
+    }
+
+    // Kural-5: İlk 10 basamak arasındaki algoritma, 11. basamağı vermelidir.
+    if(getRuleFiveStatus(value) == false){
+      returnStatus = false;
+      print("Kural-5: "+ returnStatus.toString());
+    }
+
+    print("Kurallar SON: "+ returnStatus.toString());
+
+    return returnStatus;
   }
 
-  // Kullanıcı arayüzünde girilmiş olan TCKN'nin List tipine dönüştürür. Utility metodudur.
+  // UTILITY: Kullanıcı arayüzünde girilmiş olan TCKN'nin List tipine dönüştürür.
   List getTcknCharList(String val){
     var tcknCharList = [];
     for (int i = 0; i < val.toString().length; i++) {
       tcknCharList.add(val.substring(i,i+1));
     }
     return tcknCharList;
+  }
+
+  // UTILITY: TCKN Algoritmasına göre ilk 10 rakamın toplamını int olarak geri döndürür.
+  int getTopTenTotal(List tcknCharList){
+    int total = 0;
+    for (int i = 0; i < chkCount; i++) {
+      total += int.parse(tcknCharList[i].toString());
+    }
+    return total;
+  }
+
+  // UTILITY: TCKN Algoritmasına göre ilk 9 rakamın toplamını String olarak geri döndürür.
+  String getFirstNineTotal(List tcknCharList){
+    int singleNumberTotal = 0;
+    int doubleNumberTotal = 0;
+
+    for (int i = 0; i < 9; i++) {
+      if (i % 2 == 0) {
+        singleNumberTotal += int.parse(tcknCharList[i].toString());
+      }else{ // 2,4,6,8
+        doubleNumberTotal += int.parse(tcknCharList[i].toString());
+      }
+    }
+
+    return (((singleNumberTotal*7)-doubleNumberTotal) % 10).toString();
+  }
+
+  // Kural-4 Kontrol Metodu
+  bool getRuleFourStatus(String val){
+    var tcknCharList = getTcknCharList(val);
+    if (tcknCharList.length < chkCount) return false;
+
+    return getFirstNineTotal(tcknCharList) == tcknCharList[chkCount-1];
+  }
+
+  // Kural-5 Kontrol Metodu
+  bool getRuleFiveStatus(String val){
+    List tcknCharList = getTcknCharList(val);
+    if (tcknCharList.length != chrCount) return false;
+
+    return (tcknCharList[chkCount]).toString() == (getTopTenTotal(tcknCharList) % 10).toString();
   }
 
 }
